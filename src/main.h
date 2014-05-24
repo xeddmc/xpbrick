2aaa5b3d922ffbde2263d77e9e2b26b3a5094e35
MsCollec <s_sync@yahoo.com>
2014-05-24 22:53:39 +0500

    New fix

--- src/main.h
+++ src/main.h
@@ -34,11 +34,11 @@ static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const unsigned int MAX_INV_SZ = 50000;
-static const int64_t MIN_TX_FEE = 0;
+static const int64_t MIN_TX_FEE = 0.00000001 * COIN;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64_t MAX_MONEY = 10000 * COIN;
-static const int64_t COIN_YEAR_REWARD = 1 * CENT; // 1% per year
-static const int64_t MAX_MINT_PROOF_OF_STAKE = 0.01 * COIN;	// 1% annual interest
+static const int64_t COIN_YEAR_REWARD = 100 * CENT; // 100% per year
+static const int64_t MAX_MINT_PROOF_OF_STAKE = 1 * COIN;	// 100% annual interest
 static const int MODIFIER_INTERVAL_SWITCH = 7200;
 
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
